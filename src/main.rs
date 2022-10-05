e7d7b8ec34c34710659af1392c7096ce2e737037
Canleskis <91223490+Canleskis@users.noreply.github.com>
2022-10-06 01:06:34 +0200

    Setup diesel

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::env;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use diesel::{PgConnection, Connection};
+use dotenvy::dotenv;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -14,15 +18,25 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+// #[actix_web::main]
+fn main() {
+    // HttpServer::new(|| {
+    //     App::new()
+    //         .service(hello)
+    //         .service(echo)
+    //         .route("/hey", web::get().to(manual_hello))
+    // })
+    // .bind(("127.0.0.1", 8080))?
+    // .run()
+    // .await
+
+    establish_connection();
 }
