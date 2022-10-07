12f39f501125eb0663f160768ae571e770010c9f
Faustin <faustin.boitel@bordeaux-inp.fr>
2022-10-08 00:12:29 +0200

    diesel setup

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,15 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    postes (id) {
+        id -> Int4,
+        title -> Varchar,
+        body -> Text,
+        published -> Bool,
+    }
+}
+
+diesel::table! {
     posts (id) {
         id -> Int4,
         title -> Varchar,
@@ -8,3 +17,9 @@ diesel::table! {
         published -> Bool,
     }
 }
+
+
+diesel::allow_tables_to_appear_in_same_query!(
+    postes,
+    posts,
+);
