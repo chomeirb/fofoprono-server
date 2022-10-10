917ad723321d2d1313834e69fdc2f922bf0c2409
Faustin <faustin.boitel@bordeaux-inp.fr>
2022-10-10 18:20:37 +0200

    inserting matchs

--- migrations/2022-10-08-025604_insert_games/up.sql
+++ migrations/2022-10-08-025604_insert_games/up.sql
@@ -4,3 +4,45 @@ VALUES (162, '1/8', 'France', 'Allemagne', 1, 0, 1.5, 2.5, 3.5);
 
 INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
 VALUES (162, '1/8', 'Espagne', 'Italie', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/8', 'Portugal', 'Angleterre', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/8', 'Belgique', 'Suisse', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/8', 'Pays-Bas', 'Ukraine', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/8', 'Croatie', 'Etats-Unis', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/8', 'Danemark', 'Pologne', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/8', 'Suède', 'Slovaquie', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/4', 'France', 'Espagne', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/4', 'Portugal', 'Belgique', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/4', 'Pays-Bas', 'Croatie', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/4', 'Danemark', 'Suède', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/2', 'France', 'Portugal', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, '1/2', 'Pays-Bas', 'Danemark', 1, 1, 1.5, 2.5, 3.5);
+
+INSERT INTO games (time, stage, team_home, team_away, score_home, score_away, odds_home, odds_away, odds_draw)
+VALUES (162, 'Finale', 'France', 'Pays-Bas', 1, 1, 1.5, 2.5, 3.5);
+
+
+
