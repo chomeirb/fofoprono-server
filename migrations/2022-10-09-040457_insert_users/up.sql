7b21f03e501b850df515e9fb638ab5db95dcda14
Faustin <faustin.boitel@bordeaux-inp.fr>
2022-11-15 13:30:38 +0100

    removed users and prono migration files

--- migrations/2022-10-09-040457_insert_users/up.sql
+++
@@ -1,4 +0,0 @@
--- Your SQL goes here
-INSERT INTO users (name, mail, password, active)
-    SELECT CONCAT('id', id), CONCAT('mymail@mail.com', id), CONCAT('pw', id), true
-    FROM generate_series(1, 20) id;
